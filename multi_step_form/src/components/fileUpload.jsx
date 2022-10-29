1c965fae5e766a4496cb1d85acd0863196aea051
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-10-29 13:20:54 +0530

    commets added

--- multi_step_form/src/components/fileUpload.jsx
+++ multi_step_form/src/components/fileUpload.jsx
@@ -1,57 +1,57 @@
-import React from "react";
+// import React from "react";
 
-function fileUpload() {
-  window.onload = function () {
-    //Check File API support
-    if (window.File && window.FileList && window.FileReader) {
-      var filesInput = document.getElementById("files");
+// function fileUpload() {
+//   window.onload = function () {
+//     //Check File API support
+//     if (window.File && window.FileList && window.FileReader) {
+//       var filesInput = document.getElementById("files");
 
-      filesInput.addEventListener("change", function (event) {
-        var files = event.target.files; //FileList object
-        var output = document.getElementById("imgThumbnailPreview");
+//       filesInput.addEventListener("change", function (event) {
+//         var files = event.target.files; //FileList object
+//         var output = document.getElementById("imgThumbnailPreview");
 
-        for (var i = 0; i < files.length; i++) {
-          var file = files[i];
+//         for (var i = 0; i < files.length; i++) {
+//           var file = files[i];
 
-          //Only pics
-          if (!file.type.match("image")) continue;
+//           //Only pics
+//           if (!file.type.match("image")) continue;
 
-          var picReader = new FileReader();
+//           var picReader = new FileReader();
 
-          picReader.addEventListener("load", function (event) {
-            var picSrc = event.target.result;
+//           picReader.addEventListener("load", function (event) {
+//             var picSrc = event.target.result;
 
-            var imgThumbnailElem =
-              "<div class='imgThumbContainer'><div class='IMGthumbnail' ><img  src='" +
-              picSrc +
-              "'" +
-              "title='" +
-              file.name +
-              "'/><div><span>as</span></div>";
+//             var imgThumbnailElem =
+//               "<div class='imgThumbContainer'><div class='IMGthumbnail' ><img  src='" +
+//               picSrc +
+//               "'" +
+//               "title='" +
+//               file.name +
+//               "'/><div><span>as</span></div>";
 
-            output.innerHTML = output.innerHTML + imgThumbnailElem;
-          });
+//             output.innerHTML = output.innerHTML + imgThumbnailElem;
+//           });
 
-          //Read the image
-          picReader.readAsDataURL(file);
-        }
-      });
-    } else {
-      alert("Your browser does not support File API");
-    }
-  };
-  return (
-    <div>
-      <div class="header">File API - File Reader</div>
-      <div style="padding:14px">
-        <label for="files">Select multiple files: </label>
-        <input id="files" type="file" multiple />
-      </div>
-      <div style="padding:14px; margin:auto">
-        <div id="imgThumbnailPreview"></div>
-      </div>
-    </div>
-  );
-}
+//           //Read the image
+//           picReader.readAsDataURL(file);
+//         }
+//       });
+//     } else {
+//       alert("Your browser does not support File API");
+//     }
+//   };
+//   return (
+//     <div>
+//       <div class="header">File API - File Reader</div>
+//       <div style="padding:14px">
+//         <label for="files">Select multiple files: </label>
+//         <input id="files" type="file" multiple />
+//       </div>
+//       <div style="padding:14px; margin:auto">
+//         <div id="imgThumbnailPreview"></div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default fileUpload;
+// export default fileUpload;
